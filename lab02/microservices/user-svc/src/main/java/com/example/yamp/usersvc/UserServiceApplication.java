570b4d360ac40863237398fedd64faa1302621bd
ngodat0103 <ngovuminhdat@gmail.com>
2024-11-17 18:31:46 +0700

    config(workflow): add webhook for gateway-svc

--- lab02/microservices/user-svc/src/main/java/com/example/yamp/usersvc/UserServiceApplication.java
+++ lab02/microservices/user-svc/src/main/java/com/example/yamp/usersvc/UserServiceApplication.java
@@ -3,7 +3,7 @@ package com.example.yamp.usersvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+#
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.yamp.usersvc.persistence.repository")
 public class UserServiceApplication {
